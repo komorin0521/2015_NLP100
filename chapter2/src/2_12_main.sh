699d364a107cf8f32a69739dba22abc6339a4a5c
komorin0521 <haikaradaisuki@gmail.com>
2015-06-13 18:15:38 +0900

    The shell script of 2-12

--- chapter2/src/2_12_main.sh
+++ chapter2/src/2_12_main.sh
@@ -1,2 +1,12 @@
+resultdir=../result
+if [ ! -e $resultdir ]; then
+    mkdir ../result
+fi
+col1shellfilepath=../result/col1_shell.txt
+col2shellfilepath=../result/col2_shell.txt
+cut -f 1 ../data/hightemp.txt > $col1shellfilepath
+cut -f 2 ../data/hightemp.txt > $col2shellfilepath
 python 2_12_split_and_output_colums.py \
---inputfilepath=../data/hightemp.txt
+--inputfilepath=../data/hightemp.txt \
+--col1shellfilepath=$col1shellfilepath \
+--col2shellfilepath=$col2shellfilepath
