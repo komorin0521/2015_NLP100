3b0737fbfb21b76d530df7f588aba2b09fae1cc1
komorin0521 <haikaradaisuki@gmail.com>
2015-06-13 20:51:47 +0900

    chagen the script

--- chapter2/src/2_10_main.sh
+++ chapter2/src/2_10_main.sh
@@ -1,4 +1,8 @@
 inputfilepath=../data/hightemp.txt
+if [ ! -e $inputfilepath ]; then
+    echo $inputfilepath is not found
+    exit
+fi
 lines=$(wc -l $inputfilepath | cut -d " " -f1)
 python 2_10_count_lines.py \
 --inputfilepath=$inputfilepath \
