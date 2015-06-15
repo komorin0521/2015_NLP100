4c97671c9a0d980f3b896f102b0a2af702d393d8
komorin0521 <haikaradaisuki@gmail.com>
2015-06-15 22:34:12 +0900

    modify

--- chapter2/src/2_16_split.py
+++ chapter2/src/2_16_split.py
@@ -2,6 +2,7 @@ import argparse
 import os
 import math
 
+
 def importingargs():
     """
     importing args
@@ -12,17 +13,19 @@ def importingargs():
         "--inputfilepath", help="This is the filepath of input file")
     parser.add_argument(
         "--resultshelldir", help="This is the path of result shell dir which has results files")
-    parser.add_argument("--resultpythondir", help="This is the output directory")
+    parser.add_argument(
+        "--resultpythondir", help="This is the output directory")
     parser.add_argument("--splitnum", help="split num", type=int)
     args = parser.parse_args()
     assert os.path.exists(
         args.inputfilepath), "%s is not found" % args.inputfilepath
     assert os.path.exists(
         args.resultshelldir), "%s is not found" % args.resultshelldir
-    assert os.path.exists(args.resultpythondir), "%s is not exists" % args.resultpythondir
+    assert os.path.exists(
+        args.resultpythondir), "%s is not exists" % args.resultpythondir
 
     assert args.splitnum >= 0, "The tail num must be more than zero"
- 
+
     return args.inputfilepath, args.resultshelldir, args.resultpythondir, args.splitnum
 
 
@@ -34,10 +37,11 @@ def split(inputfilepath, splitnum):
     inputlist = [line for line in open(inputfilepath, "r")]
     filenum = int(math.ceil(len(inputlist) / splitnum))
     resultlist = list()
-    resultlist = [ sorted(inputlist[i*splitnum:(i+1)*splitnum]) for i in range(0,filenum) ]
+    resultlist = [sorted(inputlist[i * splitnum:(i + 1) * splitnum])
+                  for i in range(0, filenum)]
 
     return resultlist
-        
+
 
 def output(resultshelldir, resultpythondir, resultlist):
     """
@@ -47,14 +51,15 @@ def output(resultshelldir, resultpythondir, resultlist):
     resultshelllist = list()
     for filename in filenames:
         filepath = resultshelldir + filename
-        tmpresultshelllsist = [ line for line in open(filepath,"r")]
+        tmpresultshelllsist = [line for line in open(filepath, "r")]
         resultshelllist.append(sorted(tmpresultshelllsist))
 
     #: cheking the program is correct or not
-    assert sorted(resultlist) == sorted(resultshelllist), "This program is not correct"
+    assert sorted(resultlist) == sorted(
+        resultshelllist), "This program is not correct"
 
-    for i in range(0,len(resultlist)):
-        outputfilepath = "%sresult_python%02d.txt" % (resultpythondir,i) 
+    for i in range(0, len(resultlist)):
+        outputfilepath = "%sresult_python%02d.txt" % (resultpythondir, i)
         with open(outputfilepath, "w") as outputfile:
             outputfile.write("".join(resultlist[i]))
 
